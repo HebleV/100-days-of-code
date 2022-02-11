3e194c1a213fd89a60f2c354f634819c0b8729b4
HebleV <visheble@yahoo.com>
2022-02-12 00:45:07 +0530

    Added additional sol for 2 sum

--- Programs/twoSum.js
+++ Programs/twoSum.js
@@ -10,19 +10,38 @@
 //     }
 // };
 
-var twoSum = function(nums, target) {
-    let obj = {}
-    let len = nums.length;
-    for(let i = 0;i < len; i++) {
-        let containKey = target - nums[i];
-        if(obj[containKey]!=undefined) {
-            return [obj[containKey],i]
+// const twoSum = function(nums, target) {
+//     let obj = {}
+//     let len = nums.length;
+//     for(let i = 0;i < len; i++) {
+//         let containKey = target - nums[i];
+//         if(obj[containKey]!=undefined) {
+//             return [obj[containKey],i]
+//         }
+//         obj[nums[i]] = i;
+//     }
+// };
+
+
+// O(nlog(n)) && O(1) for space
+const twoSum = (arr, target) => {
+    arr.sort = (a,b) => a - b;
+    let left = 0;
+    let right = arr.length - 1;
+    while(left < right) {
+        currentSum = arr[left] + arr[right];
+        if(currentSum === target) {
+            return [arr[left], arr[right]];
+        } else if(currentSum < target) {
+            left++;
+        } else if(currentSum > target) {
+            right--;
+        } else {
+            return [];
         }
-        obj[nums[i]] = i;
     }
-};
+}
 
+console.log(twoSum([-4,-1,1,3,5,6,8,11],13))
 
-
-console.log(twoSum([2,7,11,15],9));
 
