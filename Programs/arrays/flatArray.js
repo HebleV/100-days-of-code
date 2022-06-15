dec551b50a2f54a95966485cc50eb53e3a939c33
HebleV <visheble@yahoo.com>
2022-06-15 20:09:29 +0530

    Added brute force pgm for flattening arr

--- Programs/arrays/flatArray.js
+++ Programs/arrays/flatArray.js
@@ -2,11 +2,34 @@
 //    return arr.flat();
 // }
 
-let newArr = [];
-function flatArr(arr) {
-    newArr = arr.reduce((prev, curr) => {
-        return prev.concat(curr);
-    }, [])
-}
-flatArr([1, [2, 3]]);
-console.log(newArr);
+// let newArr = [];
+const flatArr = (arr) => {
+  return arr.reduce((prev, curr) => {
+    if (Array.isArray(curr)) {
+      return prev.concat(flatArr(curr));
+    }
+    return prev.concat(curr);
+  }, []);
+};
+
+
+//Brute force method
+
+// function flatArr(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!Array.isArray(arr[i])) {
+//       newArr.push(arr[i]);
+//     } else {
+//       for (let j = 0; j < arr[i].length; j++) {
+//         if (!Array.isArray(arr[i][j])) {
+//           newArr.push(arr[i][j]);
+//         }
+//         flatArr(arr[i][j]);
+//       }
+//     }
+//   }
+//   return newArr;
+// }
+// flatArr([1, [2, 3],[4,5,[6,7,[[8,9]]]]]);
+console.log(flatArr([1, 2, [3, 4], [5, [[6, 7]]]]));
+// console.log(newArr);
