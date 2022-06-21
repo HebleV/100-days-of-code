f10f0282dc79448fa7d935215017bcc4e079180b
HebleV <visheble@yahoo.com>
2022-06-21 21:22:32 +0530

    Updated flattening arr

--- Programs/arrays/flatArray.js
+++ Programs/arrays/flatArray.js
@@ -3,33 +3,33 @@
 // }
 
 // let newArr = [];
-const flatArr = (arr) => {
-  return arr.reduce((prev, curr) => {
-    if (Array.isArray(curr)) {
-      return prev.concat(flatArr(curr));
-    }
-    return prev.concat(curr);
-  }, []);
-};
+// const flatArr = (arr) => {
+//   return arr.reduce((prev, curr) => {
+//     if (Array.isArray(curr)) {
+//       return prev.concat(flatArr(curr));
+//     }
+//     return prev.concat(curr);
+//   }, []);
+// };
 
 
 //Brute force method
 
-// function flatArr(arr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     if (!Array.isArray(arr[i])) {
-//       newArr.push(arr[i]);
-//     } else {
-//       for (let j = 0; j < arr[i].length; j++) {
-//         if (!Array.isArray(arr[i][j])) {
-//           newArr.push(arr[i][j]);
-//         }
-//         flatArr(arr[i][j]);
-//       }
-//     }
-//   }
-//   return newArr;
-// }
-// flatArr([1, [2, 3],[4,5,[6,7,[[8,9]]]]]);
+function flatArr(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (!Array.isArray(arr[i])) {
+      newArr.push(arr[i]);
+    } else {
+      for (let j = 0; j < arr[i].length; j++) {
+        if (!Array.isArray(arr[i][j])) {
+          newArr.push(arr[i][j]);
+        }
+        flatArr(arr[i][j]);
+      }
+    }
+  }
+  return newArr;
+}
+flatArr([1, [2, 3],[4,5,[6,7,[[8,9]]]]]);
 console.log(flatArr([1, 2, [3, 4], [5, [[6, 7]]]]));
 // console.log(newArr);
