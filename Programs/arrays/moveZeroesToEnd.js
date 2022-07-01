5862df79b051f3f0b0571dea3fba9f47884f9bc1
HebleV <visheble@yahoo.com>
2022-07-01 23:27:37 +0530

    Added pgm for moving zeroes to end in arr using Lomuto partition technique

--- Programs/arrays/moveZeroesToEnd.js
+++ Programs/arrays/moveZeroesToEnd.js
@@ -1,16 +1,31 @@
-function moveZeroes(arr){
-    let i = 0;
-    let j = arr.length - 1;
-    while(i < j) {
-        while(i < j && arr[j] === 0){
-            j--;
-        }
-        if(arr[i] === 0){
-            [arr[i],arr[j]] = [arr[j],arr[i]]
-        }
-        i++;
+// function moveZeroes(arr){
+//     let i = 0;
+//     let j = arr.length - 1;
+//     while(i < j) {
+//         while(i < j && arr[j] === 0){
+//             j--;
+//         }
+//         if(arr[i] === 0){
+//             [arr[i],arr[j]] = [arr[j],arr[i]]
+//         }
+//         i++;
+//     }
+//     return arr
+// }
+
+
+// Lomuto technique
+function moveZeroes(arr) {
+  let i = -1;
+  let j = 0;
+  while (j < arr.length) {
+    if (arr[j] !== 0) {
+      i++;
+      [arr[i], arr[j]] = [arr[j], arr[i]];
     }
-    return arr
+    j++;
+  }
+  return arr;
 }
 
-console.log(moveZeroes([1,0,2,3,0,0,4,0,5,6]))
+console.log(moveZeroes([1, 0, 2, 3, 0, 0, 4, 0, 5, 6]));
