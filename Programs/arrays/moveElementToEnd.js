498dd5eb8da294681223a8e50f907ff5759942a1
HebleV <visheble@yahoo.com>
2022-07-01 23:28:59 +0530

    Added pgm for moving ele to end in arr using Lomuto partition technique

--- Programs/arrays/moveElementToEnd.js
+++ Programs/arrays/moveElementToEnd.js
@@ -1,16 +1,31 @@
-function moveElementToEnd(array, toMove) {
-    let i = 0;
-    let j = array.length - 1;
-    while(i < j) {
-        while(i < j && array[j] === toMove) {
-            j--;
-        }
-        if(array[i] === toMove) {
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        i++;
+// function moveElementToEnd(array, toMove) {
+//     let i = 0;
+//     let j = array.length - 1;
+//     while(i < j) {
+//         while(i < j && array[j] === toMove) {
+//             j--;
+//         }
+//         if(array[i] === toMove) {
+//             [array[i], array[j]] = [array[j], array[i]];
+//         }
+//         i++;
+//     }
+//     return array;
+// }
+
+
+// Lomuto technique
+function moveElementToEnd(arr, toMove) {
+  let i = -1;
+  let j = 0;
+  while (j < arr.length) {
+    if (arr[j] !== toMove) {
+      i++;
+      [arr[i], arr[j]] = [arr[j], arr[i]];
     }
-    return array;
+    j++;
+  }
+  return arr;
 }
 
-console.log(moveElementToEnd([2,1,2,2,2,3,4,2], 2))
+console.log(moveElementToEnd([2, 1, 2, 2, 2, 3, 4, 2], 2));
