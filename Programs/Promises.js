a935ca84fe89defc601dcb2b563727f344313430
HebleV <visheble@yahoo.com>
2022-07-09 20:15:11 +0530

    Testing promises

--- Programs/Promises.js
+++ Programs/Promises.js
@@ -12,26 +12,41 @@
 //     console.log('Nah, ' + error);
 // })
 
-let p1 = new Promise((resolve, reject) => {
-    return resolve('done with p1');
-})
+// let p1 = new Promise((resolve, reject) => {
+//     return resolve('done with p1');
+// })
 
-let p2 = new Promise((resolve,reject)=>{
-    return resolve('done with p2');
-})
+// let p2 = new Promise((resolve,reject)=>{
+//     return resolve('done with p2');
+// })
 
-let p3 = new Promise((resolve,reject) => {
-    return resolve('done with p3');
-})
+// let p3 = new Promise((resolve,reject) => {
+//     return resolve('done with p3');
+// })
 
-let p4 = Promise.all([p1, p2, p3]);
-console.log(p4);
+// let p4 = Promise.all([p1, p2, p3]);
+// console.log(p4);
 
+//   for (var i = 0; i < 3; i++) {
+//     const log = () => {
+//       console.log(i);
+//     }
+//     setTimeout(log, 100);
+//   }
 
-  for (var i = 0; i < 3; i++) {
-    const log = () => {
-      console.log(i);
+function check(n) {
+  let p = new Promise((resolve, reject) => {
+    if (n % 2 === 0) {
+      return resolve("Even");
+    } else {
+      return reject("Odd");
     }
-    setTimeout(log, 100);
-  }
-  
+  });
+  p.then(() => {
+    console.log("Even");
+  }).catch(() => {
+    console.log("Odd");
+  });
+}
+
+console.log(check(2));
