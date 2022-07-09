2c04d31b0ba7b5f1b5c3ac186d25317821069150
HebleV <visheble@yahoo.com>
2022-07-09 20:12:46 +0530

    Added pgm for first non repeating ele

---
+++ Programs/arrays/firstNonRepeatingElement.js
@@ -0,0 +1,15 @@
+//Using hashmap => O(n) && O(n)
+function firstNonRepeatingElement(arr) {
+  const countObj = {};
+  for (let ele of arr) {
+    countObj[ele] = countObj[ele] + 1 || 1;
+  }
+  for (let count in countObj) {
+    if (countObj[count] === 1) {
+      return count;
+    }
+  }
+  return -1;
+}
+
+console.log(firstNonRepeatingElement([9, 4, 9, 6, 7, 4]));
