88b0f7a4092b30e9476dd594eec0d8134bb6f438
HebleV <visheble@yahoo.com>
2022-07-11 22:08:17 +0530

    Added pgm for first non repeating char

--- Programs/strings/firstNonRepeatingCharacter.js
+++ Programs/strings/firstNonRepeatingCharacter.js
@@ -1,9 +1,22 @@
+// function noRepeat(str){
+//     for(let i = 0;i<str.length;i++){
+//         if(str.indexOf(str.charAt(i)) === str.lastIndexOf(str.charAt(i))){
+//             return str.charAt(i);
+//         }
+//     }
+// }
+
 function noRepeat(str){
-    for(let i = 0;i<str.length;i++){
-        if(str.indexOf(str.charAt(i)) === str.lastIndexOf(str.charAt(i))){
-            return str.charAt(i);
-        }
+    const countChar = {};
+    for(let char of str){
+        countChar[char] = countChar[char] + 1 || 1;
     }
+    for(let char in countChar){
+        if(countChar[char] === 1){
+            return char
+        } 
+    }
+    return -1;
 }
 let str = 'aabbccdeeff';
 console.log(noRepeat(str));
