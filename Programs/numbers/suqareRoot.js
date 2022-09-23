692581d18393c000e2a9fb2ab51a47522ff7c7f7
HebleV <visheble@yahoo.com>
2022-09-23 12:22:21 +0530

    Pgm for perfect sq num

--- Programs/numbers/suqareRoot.js
+++ Programs/numbers/suqareRoot.js
@@ -3,10 +3,10 @@
 function sqRoot(A){
     for(let i = 1; i <= A; i++){
         if(i * i === A) {
-            return i;
+            return 1;
         } 
     }
-    return -1;
+    return 0;
 }
 
-console.log(sqRoot(25))
+console.log(sqRoot(1))
