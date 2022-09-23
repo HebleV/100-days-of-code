728bf8fce0b197c9f2141992de287ae70fb1f299
HebleV <visheble@yahoo.com>
2022-09-23 17:46:57 +0530

    Optimised pgm for square root

--- Programs/numbers/suqareRoot.js
+++ Programs/numbers/suqareRoot.js
@@ -2,11 +2,14 @@
 
 function sqRoot(A){
     for(let i = 1; i <= A; i++){
+        console.log("i",i)
         if(i * i === A) {
             return 1;
-        } 
+        } else if(i * i >= A) {
+            break;
+        }
     }
     return 0;
 }
 
-console.log(sqRoot(1))
+console.log(sqRoot(18))
