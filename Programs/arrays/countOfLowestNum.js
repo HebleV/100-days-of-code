7d433ee69f6a1eafe43e1f2d2095a6be93d1c2e9
HebleV <visheble@yahoo.com>
2022-09-29 23:13:12 +0530

    Updated soln for count of lowest num

--- Programs/arrays/countOfLowestNum.js
+++ Programs/arrays/countOfLowestNum.js
@@ -1,17 +1,35 @@
+// function countOfLowestNum(arr){
+//     let max = 0;
+//     let maxCount = 0;
+//     for(let i = 0; i <arr.length; i++){
+//         if(arr[i] > max && arr[i] !== max){
+//             max = arr[i];
+//             console.log("max", max)
+//         }
+//     }
+//     for(let i = 0; i <arr.length; i++){
+//         if(max === arr[i]){
+//             console.log("max 1", max)
+//                 maxCount++;
+//             console.log("maxCount", maxCount)
+//         }
+//     }
+//     return arr.length - maxCount;
+// }
+
 function countOfLowestNum(arr){
     let max = 0;
     let maxCount = 0;
-    for(let i = 0; i <= arr.length; i++){
+    for(let i = 0; i<arr.length; i++){
         if(arr[i] > max){
-            console.log("yyyy")
             max = arr[i];
-            console.log("max", max)
-            // if(max === arr[i]){
-                maxCount++;
-            // }
+            maxCount = 1;
+        } else if(arr[i] === max){
+            maxCount++;
         }
     }
     return arr.length - maxCount;
 }
 
-console.log(countOfLowestNum([3,1,2]))
+console.log(countOfLowestNum([3,1,2,0,4,6,5,6,7]))
+// console.log(countOfLowestNum([2,5,0,-1,6,3,7,6,5,7]))
