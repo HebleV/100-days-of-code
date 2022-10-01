eafd30dc4e5ac62e1901917bf97647f76290a39d
HebleV <visheble@yahoo.com>
2022-10-01 23:28:54 +0530

    Added pgm for counting occurence

--- Programs/arrays/countOccurencesOfAElement.js
+++ Programs/arrays/countOccurencesOfAElement.js
@@ -1,23 +1,23 @@
 //Using linear search => O(n) && O(1)
-// function countOccurencesOfAElement(arr,n){
-//     let count = 0;
-//     for(let ele of arr) {
-//         if(ele === n){
-//             count++;
-//         } 
-//     }
-//     return count;
-// }
-
-function countOccurencesOfAElement(arr,n) {
-    arr.sort((a,b) => a-b);
-    let mid = Math.floor(arr.length/2);
-    if(n === arr[mid]) {
-        return mid
+function countOccurencesOfAElement(arr,n){
+    let count = 0;
+    for(let ele of arr) {
+        if(ele === n){
+            count++;
+        } 
     }
-    if(arr[mid] > n){
-
-    }
+    return count;
 }
 
-console.log(countOccurencesOfAElement([1,2,3,2,4,2,5,2],2))
+// function countOccurencesOfAElement(arr,n) {
+//     arr.sort((a,b) => a-b);
+//     let mid = Math.floor(arr.length/2);
+//     if(n === arr[mid]) {
+//         return mid
+//     }
+//     if(arr[mid] > n){
+
+//     }
+// }
+
+console.log(countOccurencesOfAElement([1,2,2],2))
