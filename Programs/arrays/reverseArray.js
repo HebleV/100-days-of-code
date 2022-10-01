8178ebeea48849c2b9a8688a8e3e100a0169773b
HebleV <visheble@yahoo.com>
2022-10-01 23:30:00 +0530

    Added pgm for reversin arr

--- Programs/arrays/reverseArray.js
+++ Programs/arrays/reverseArray.js
@@ -1,10 +1,23 @@
-const reverseArray = array => {
-    for (let i = 0; i < array.length / 2; i++) {
-        const temp = array[i];
-        array[i] = array[array.length - 1 - i];
-        array[array.length - 1 - i] = temp;
+// const reverseArray = array => {
+//     for (let i = 0; i < array.length / 2; i++) {
+//         const temp = array[i];
+//         array[i] = array[array.length - 1 - i];
+//         array[array.length - 1 - i] = temp;
+//     }
+//     return array;
+// };
+
+const reverseArray = (arr) => {
+    let i = 0;
+    let j = arr.length - 1;
+    while(i <= j){
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
     }
-    return array;
-};
+    return arr;
+}
 
 console.log(reverseArray([1,2,3,4]));
