4f457c963f56b2a38a0fe894d513e294437f6e29
HebleV <visheble@yahoo.com>
2022-11-02 22:25:16 +0530

    Added pgm for boundary ele

---
+++ Programs/arrays/2D-Matrices/boundaryElements.js
@@ -0,0 +1,33 @@
+//Print only the boundary elements
+function boundaryElements(arr){
+    let row = 0;
+    let col = 0;
+    const newArr = [];
+    for(let i = 0; i<arr.length-1; i++){
+        newArr.push(arr[row][col]);
+        col++;
+    }
+    for(let i = 0; i<arr.length-1; i++){
+        newArr.push(arr[row][col]);
+        row++;
+    }
+    for(let i = 0; i<arr.length-1; i++){
+        newArr.push(arr[row][col]);
+        col--;
+    }
+    for(let i = 0; i<arr.length-1; i++){
+        newArr.push(arr[row][col]);
+        row--;
+    }
+    return newArr;
+}
+
+const arr1 = [
+    [1,2,3,4,5,6],
+    [7,8,9,10,11,12],
+    [13,14,15,16,17,18],
+    [19,20,21,22,23,24],
+    [25,26,27,28,29,30],
+    [31,32,33,34,35,36]
+];
+console.log(boundaryElements(arr1))
