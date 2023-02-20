b1630657cbb28d2fefa0a2afeabd11f8a209c95f
HebleV <visheble@yahoo.com>
2023-02-20 21:32:36 +0530

    Added pgm for max subarr sum

--- Programs/arrays/1-Dimensional/maxSumSubArr.js
+++ Programs/arrays/1-Dimensional/maxSumSubArr.js
@@ -1,6 +1,6 @@
 // Find the contiguous non-empty subarray within an array, A of length N, 
 // with the largest sum.
-//Using Kadane's algo
+//Using Kadane's algo - O(n)
 function maxSumSubArr(A) {
   // let max = Number.MIN_VALUE;
   // let sum = 0;
@@ -22,5 +22,37 @@ function maxSumSubArr(A) {
   return max;
 }
 
+// Using bruteforce - O(n3)
+function maxSumSubArr1(arr){
+  // let max_sum = Number.NEGATIVE_INFINITY;
+  // let sum;
+  // for(let s = 0; s<arr.length; s++){
+  //   for(let e = s; e<arr.length; e++){
+  //     sum = 0;
+  //     for(let i = s; i<=e; i++){
+  //       sum = sum + arr[i];
+  //     }
+  //     max_sum = Math.max(max_sum, sum);
+  //   }
+  // }
+  // return max_sum;
+
+  let max_sum = Number.NEGATIVE_INFINITY;
+  let sum;
+  for(let i = 0; i<arr.length; i++){
+    for(let j = i; j<arr.length; j++){
+      sum = 0;
+      for(let k = i; k<=j; k++){
+
+      sum = sum + arr[k];
+      console.log("sum", sum)
+      max_sum = Math.max(max_sum,sum);
+      console.log("max_sum", max_sum)
+    }
+    }
+  }
+  return max_sum;
+}
 const A1 = [1, 2, 3, 4, -10];
-console.log(maxSumSubArr(A1));
+const A2 = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+console.log(maxSumSubArr1(A2));
