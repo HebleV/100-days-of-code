37530d0d3a4693e24ecaacfad3ab199654c7669d
HebleV <visheble@yahoo.com>
2023-03-10 22:06:41 +0530

    Added pgm for subarr with 0 sum

--- Programs/hashmap/subArrayWith0Sum.js
+++ Programs/hashmap/subArrayWith0Sum.js
@@ -1,23 +1,36 @@
+// function subArrayWith0Sum(arr){
+//     let sum = 0;
+//     let hashMap = new Map();
+//     hashMap.set(0);
+//     console.log("hashMap",hashMap)
+//     for(let i = 0; i<arr.length; i++){
+//         sum = sum + arr[i];
+//         console.log("sum", sum, arr[i])
+//         if(hashMap.has(sum)){
+//             return 1
+//         } else {
+//             hashMap.set(sum);
+//         }
+//     }
+//     console.log("hashMap 2",hashMap)
+
+//     return 0
+// }
+
 function subArrayWith0Sum(arr){
-    let sum = 0;
-    let hashMap = new Map();
-    hashMap.set(0);
-    console.log("hashMap",hashMap)
     for(let i = 0; i<arr.length; i++){
-        sum = sum + arr[i];
-        console.log("sum", sum, arr[i])
-        if(hashMap.has(sum)){
-            return 1
-        } else {
-            hashMap.set(sum);
+        let sum = 0;
+        for(let j = i; j<arr.length; j++){
+            sum = sum + arr[j];
+            if(sum === 0){
+                return 1;
+            } 
         }
     }
-    console.log("hashMap 2",hashMap)
-
     return 0
 }
 
 const arr1 = [1, 2, 3,4, 5];
 const arr2 = [1,-1];
 
-console.log(subArrayWith0Sum(arr1))
+console.log(subArrayWith0Sum(arr2))
